6acb182030fa435511c6a50626267d7a462dcb7a
Avaneesh <avaneeshkad@gmail.com>
2026-01-29 15:34:03 -0500

    Add files via upload

---
+++ ExponentialValue.py
@@ -0,0 +1,11 @@
+import math
+
+x = int(input("Enter the number of which the exponential is supposed to be calculated : "))
+
+e_x = []
+for i in range(0,11):
+    e_x.append(x ** i / math.factorial(i))
+
+print("Number of Terms:\tEstimates: ")
+for i in range(len(e_x)-1):
+    print("{0} \t\t {1}".format(i, e_x[i]))
